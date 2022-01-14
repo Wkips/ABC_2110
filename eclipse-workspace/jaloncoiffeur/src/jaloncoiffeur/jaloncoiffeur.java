bda015dce9a4af825f96f6c59aca053ab6199db0
WillyWgreen <willy_kips@hotmail.fr>
2022-01-14 11:10:51 +0100

    Update jaloncoiffeur.java

--- eclipse-workspace/jaloncoiffeur/src/jaloncoiffeur/jaloncoiffeur.java
+++ eclipse-workspace/jaloncoiffeur/src/jaloncoiffeur/jaloncoiffeur.java
@@ -7,37 +7,44 @@ public class jaloncoiffeur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//declaration des variables		
-String etudiant;
-String mercredi;
-String jeudi;
+		//declaration des variables		
+		String etudiant;
+		String mercredi;
+		String jeudi;
 
-System.out.println("Etes vous etudiants   oui / non ");
-Scanner sc = new Scanner (System.in);
-etudiant = sc.next();
+		System.out.println("Etes vous etudiants   oui / non ");
+		Scanner sc = new Scanner (System.in);
+		etudiant = sc.next();
 
-System.out.println("sommes nous mercredi  oui / non ");
-mercredi = sc.next();
-if (mercredi.equals("oui")){
- System.out.println("Vouas avez 50%");
-}
+		if  (etudiant.equals("oui")) {
 
-System.out.println("sommes nous jeudi  oui / non ");
-jeudi = sc.next(); 
-if (jeudi.equals("oui")){
+			System.out.println("sommes nous mercredi  oui / non ");
+			mercredi = sc.next();
+
+			if (mercredi.equals("oui")){
+				System.out.println("Vouas avez 50%");
+			}
+			else if (mercredi.equals("non")){
+				System.out.println("vous avez pas reduction");}
+
+
+
+
+			System.out.println("sommes nous jeudi  oui / non ");
+			jeudi = sc.next(); 
+			if (jeudi.equals("oui"))
+
+				System.out.println("vous avez  20%");
+
+			else if (jeudi.equals("non")){
+				System.out.println("vous avez pas reduction");}
+
+		}
 
 	
-System.out.println("vous avez  20%");
 
+	System.out.println("sortez la CB");
+}
 }
 
-else if (etudiant.equals("non")) {
-System.out.println("sortez la CB");
-}
-
-}
-
-
-
-	}
 
