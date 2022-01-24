a05fd225411a62603a78108f520ffa0a43d67221
WillyWgreen <willy_kips@hotmail.fr>
2022-01-24 15:59:28 +0100

    ..

---
+++ eclipse-workspace/9.4 nombre reel/src/reel.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class reel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+//valeur absolue d un nombre reel
+float nombre1;
+Scanner sc = new Scanner (System.in) ;
+
+	System.out.println("entrez svp nombre 1");
+	nombre1=sc.nextFloat();
+	
+	|x| = x, si x< 0 ;
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+}
+}
