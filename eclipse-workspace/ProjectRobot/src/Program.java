aabb56bb375e05659fced0a056de5ddcb113c76e
WillyWgreen <willy_kips@hotmail.fr>
2022-01-28 11:53:31 +0100

    robot

--- eclipse-workspace/ProjectRobot/src/Program.java
+++ eclipse-workspace/ProjectRobot/src/Program.java
@@ -1,9 +1,18 @@
 
 public class Program {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int a= 3;
+        Scanner sc = new Scanner(System.in);
+        Robot robotParDefault = new Robot();
+        Robot robotParDefault2 = new Robot();
+        Robot robotParDefault3 = new Robot();
+        Robot robotNono= new Robot("1a252",true,"roue",true,0,0,0);
+        /*boolean ilfaitbeauAujourdhui=true;
+        boolean lerobotvabien = robotNono.caVa(ilfaitbeauAujourdhui);
+        ilfaitbeauAujourdhui=false;
+        boolean lerobotvabien2 = robotNono.caVa(ilfaitbeauAujourdhui);*/
+        boolean areussi = robotNono.demarrer();
+        boolean areussiPour3= robotParDefault3.demarrer();
+    }
 }
--- eclipse-workspace/ProjectRobot/src/Robot.java
+++ eclipse-workspace/ProjectRobot/src/Robot.java
@@ -1,4 +1,61 @@
-
+import java.util.Scanner;
 public class Robot {
-
+    private String numeroDeSerie;
+    private boolean enMarche;
+    private String typeDeDeplacement;
+    private boolean aDesBras;
+    private float positionX;
+    private float positionY;
+    private float positionZ;
+    //constructeupar default: il donne des valeurs par defaut aux attributs
+    public Robot() {
+        numeroDeSerie="O";
+        enMarche=false;
+        typeDeDeplacement="DeplacementPied";
+        aDesBras=true;
+        positionX=0;
+        positionY=0;
+        positionZ=0;
+    }
+    //constructeur classique: il donne les valeurs passées en parametre aux attributs
+    public Robot(String numeroDeSerie,
+            boolean _enMarche,
+            String _typeDeDeplacement,
+            boolean aDesBras,
+            float _positionX,
+            float _positionY,
+            float _positionZ) {
+        this.numeroDeSerie=numeroDeSerie;
+        enMarche=_enMarche;
+        typeDeDeplacement=_typeDeDeplacement;
+        this.aDesBras=aDesBras;
+        positionX=_positionX;
+        positionY=_positionY;
+        positionZ=_positionZ;
+    }
+    //fonction: il renvoie un boolean, ici true
+    public boolean caVa(boolean ilfaitbeau) {
+        if (ilfaitbeau==true) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //procedure: ne renvoie rien (void=rien)
+    public void caVa() {
+    }
+    public boolean demarrer()
+    {
+        if (enMarche==true)
+        {
+            return false;
+        }
+        else
+        {
+            enMarche=true;
+            return true;
+        }
+    }
 }
