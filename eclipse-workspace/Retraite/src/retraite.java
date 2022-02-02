84b38d64a63709442cf559500d15585913b3ab49
WillyWgreen <willy_kips@hotmail.fr>
2022-02-02 09:23:20 +0100

    retraite end

---
+++ eclipse-workspace/Retraite/src/retraite.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class retraite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int age;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("veuillez saisir votre age");
+		age = sc.nextInt();
+		
+		// getRetired recupere le resultat du calcul/fonction 
+		String resultat= getRetired(age);
+		System.out.println(resultat);
+	}
+	//creer la classe
+
+
+	public static String getRetired(int ageActuel){		
+		//traitement calcul
+		if (ageActuel>60){
+			int differenceAge = (ageActuel-60);
+			return"vous etes a la retraite depuis" + differenceAge + "annee";
+		}
+		if (ageActuel==60) {
+			return "vous etes a la retraite";
+		}
+		if (ageActuel<0) {
+			return " vous etes pas né";
+		}
+		else {
+			int resteATravailler=(60-ageActuel);
+			return "vous devez encore travailer"+resteATravailler+"annee";
+		}
+		
+	}
+
+}
+
+
+
--- eclipse-workspace/coursfonction/src/coursfonction/cours.java
+++ eclipse-workspace/coursfonction/src/coursfonction/cours.java
@@ -4,7 +4,41 @@ public class cours {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		public class ObjetCoursTest {
 
+
+
+			public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+
+
+			int ageATester =17;
+			String resultat = estIlMajeur (ageATester);
+			System.out.println(resultat);
+			}// Fin Main
+
+
+
+			// creation de fonction qui renvois une valeur ( methode de classe)
+
+
+
+			public static String estIlMajeur (int age) {
+
+
+
+			if (age>=18){
+			return "Majeur!";
+			}
+			else {
+			return "Mineur!";
+			}
+			}// Fin fonction
+
+
+
+			}//Fin Class
 	}
 
 }
