719e39f741d087b0deceeae63dcb9c668f5e20d7
WillyWgreen <willy_kips@hotmail.fr>
2022-02-14 16:24:04 +0100

    Create whileboucle.java

---
+++ eclipse-workspace/TableauCodcademy/src/whileboucle.java
@@ -0,0 +1,22 @@
+
+public class whileboucle {
+
+	class Coffee {
+		  
+		  public static void main(String[] args) {
+		    
+		    // initialize cupsOfCoffee
+		    int cupsOfCoffee = 1;
+		    
+		    // add while loop with counter
+		    while (cupsOfCoffee <= 100) {
+		      
+		      System.out.println("nombre de cafe " + cupsOfCoffee);
+		      cupsOfCoffee++;
+		      
+		    }
+		    
+		  }
+		  
+		}
+}
