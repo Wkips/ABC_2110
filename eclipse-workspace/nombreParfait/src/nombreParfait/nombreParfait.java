76d6131678b078d998599afefc45034c4781cfa4
WillyWgreen <willy_kips@hotmail.fr>
2022-02-18 11:53:21 +0100

    parfait

---
+++ eclipse-workspace/nombreParfait/src/nombreParfait/nombreParfait.java
@@ -0,0 +1,31 @@
+package nombreParfait;
+
+import java.util.Scanner;
+
+public class nombreParfait {
+	Scanner sc = new Scanner (System.in);
+	int nombre1;
+	int somme;
+	int i;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("recherche des nombres parfait");
+
+System.out.println("Combien de nombres voulez vous afficher");
+
+		
+		
+	}
+
+}
+/*On souhaite écrire un programme de calcul des Npremiers nombres parfaits.
+ *  Un nombre est dit parfait s’il est égal à la somme de ses diviseurs, 1 compris.  
+ *  Exemple:6= 1+2+3 6estun nombre parfait.  
+ *  L'algorithme  retenu  contiendra  deux  boucles  imbriquées.  Une  boucle  de  comptage  des  nombres  parfaits  
+ *  qui s'arrêtera lorsque le décompte sera atteint, la boucle interne ayant vocation à calculer tous les diviseurs du
+ *   nombre examiné d'en faire la somme puis detester l'égalité entre cette somme et le nombre.   
+ *   
+ *   Ecrivez le programme complet qui afficheles N premiers nombres parfaits.Exemple d’affichage en mode Console:Programme de
+ *    recherche des nombresparfaits.Combien de nombre parfaits souhaitez-vous afficher?4  
+ */
