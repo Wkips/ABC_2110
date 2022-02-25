46802b39e8cddd25da39205b981a9eb1a1773225
WillyWgreen <willy_kips@hotmail.fr>
2022-02-25 09:04:59 +0100

    Update nombreParfait.java

--- eclipse-workspace/nombreParfait/src/nombreParfait/nombreParfait.java
+++ eclipse-workspace/nombreParfait/src/nombreParfait/nombreParfait.java
@@ -3,27 +3,47 @@ package nombreParfait;
 import java.util.Scanner;
 
 public class nombreParfait {
-	Scanner sc = new Scanner (System.in);
-	int nombre1;
-	int somme;
-	int i;
-	
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("recherche des nombres parfait");
 
-System.out.println("Combien de nombres voulez vous afficher");
+		Scanner sc = new Scanner (System.in);
+		int combien;
+		int nombre;
+		int somme = 0;
 
-		
-		
+
+		System.out.println("Combien de nombres voulez vous afficher");
+		combien = sc.nextInt();
+		for (int i = 0;i<=combien;i++) 		
+		{
+
+			
+
+		nombre = sc.nextInt();
+		for (i=1; i<nombre;i++) 
+		{
+
+			if (nombre%i==0) 
+			{
+				System.out.println(i);
+			}
+
+			}
+		}
+
 	}
-
 }
+
+
 /*On souhaite écrire un programme de calcul des Npremiers nombres parfaits.
  *  Un nombre est dit parfait s’il est égal à la somme de ses diviseurs, 1 compris.  
  *  Exemple:6= 1+2+3 6estun nombre parfait.  
- *  L'algorithme  retenu  contiendra  deux  boucles  imbriquées.  Une  boucle  de  comptage  des  nombres  parfaits  
- *  qui s'arrêtera lorsque le décompte sera atteint, la boucle interne ayant vocation à calculer tous les diviseurs du
+ *  L'algorithme  retenu  contiendra  deux  boucles  imbriquées. 
+ *   Une  boucle  de  comptage  des  nombres  parfaits  
+ *  qui s'arrêtera lorsque le décompte sera atteint,
+ *   la boucle interne ayant vocation à calculer tous les diviseurs du
  *   nombre examiné d'en faire la somme puis detester l'égalité entre cette somme et le nombre.   
  *   
  *   Ecrivez le programme complet qui afficheles N premiers nombres parfaits.Exemple d’affichage en mode Console:Programme de
