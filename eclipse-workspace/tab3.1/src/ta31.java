9f39d1bcd58097542773b1ce21eecb01e8ba97fe
WillyWgreen <willy_kips@hotmail.fr>
2022-03-30 16:54:28 +0200

    Update ta31.java

--- eclipse-workspace/tab3.1/src/ta31.java
+++ eclipse-workspace/tab3.1/src/ta31.java
@@ -6,18 +6,28 @@ public class ta31 {
 		// TODO Auto-generated method stub
 		/*Soitun tableau de nombres triés par ordre croissant.Chercher si un nombre donné Nfigure parmi les éléments. Si oui, indiquer la valeur de l'indice
 		correspondant.Sinon, indiquer «Non trouve"*/
-		
-		int [] nombres	= {1, 2, 3, 4};
-	int n;
-	
-	Scanner sc = new Scanner(System.in);
-	
-	System.out.println("Veuillez saisir un nombre");
-	
-	n=sc.nextInt();
-	
-	
-		
-	}
 
+		int [] nombres	= {1, 2, 3, 4};
+		int nbSaisie;
+
+		Scanner sc = new Scanner(System.in);
+
+
+
+
+
+		for  (int i=0; i<nombres.length; i++)	{
+			//System.out.println(nombres[i]);	
+
+
+			System.out.println("Veuillez saisir un nombre");
+			nbSaisie=sc.nextInt();
+			if (nbSaisie == nombres[i++]) {
+				System.out.println("correspondant");
+			}
+			else {
+				System.out.println("Non trouve");
+			}
+		}
+	}
 }
